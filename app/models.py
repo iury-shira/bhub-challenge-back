9a3a54d7f521ca1b71074cab301462bc23ad2024
iury-shira <iuryresende628@gmail.com>
2023-01-24 10:06:52 -0300

    Create database and orm config files

--- app/models.py
+++ app/models.py
@@ -1,1 +1,15 @@
-  
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+from .database import Base
+
+class Client(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    corporate_name = Column(String)
+    phone_number = Column(String)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    declared_billing = Column(Integer)
